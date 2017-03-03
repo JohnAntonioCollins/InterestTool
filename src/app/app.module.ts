276ee388564aa893acc7b19e790d7c4b5a068a6e
John Collins <johncollins@zipcoders-MacBook-Pro-4.local>
2017-03-03 14:23:52 -0500

    adding existing files from old repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
+import { ComplexPage } from '../pages/complex/complex';
+import {SimplePage } from '../pages/simple/simple';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    ComplexPage,
+    SimplePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    ComplexPage,
+    SimplePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}

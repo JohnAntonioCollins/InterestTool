6bd788c43177a37d356d918a81d68525a4afb6c1
wmattern0 <wm.m.mattern@gmail.com>
2017-03-03 16:17:27 -0500

    broke up 2 posts

--- src/services/interest-service.ts
+++ src/services/interest-service.ts
@@ -6,33 +6,41 @@ export class InterestService {
 
     private data: any;
     private responseData: any;
+    private id:any;
     private interestValue: any;
     private url: string = "http://127.0.0.1:8080/interestCalculator";
 
     constructor(private http: Http) {}
 
-    getInterestValue(balance: number, accountType: string, interestRate:number, overDraftPenalty:number,
-    requiredMinimumBalance:number, isMinimumBalanceRequired:boolean, recurringTransactions:[any], accountHistory:[any],
-     interval:number, frequency:number,interestType:number, calculationRule:number, numDaysForRule:number) {
+    postAccount(balance: number, accountType: string, interestRate:number, overDraftPenalty:number,
+    requiredMinimumBalance:number, isMinimumBalanceRequired:boolean, recurringTransactions:[any], accountHistory:[any]){
         this.data = {
-            "account": {
-                "balance": balance,
-                "accountType": accountType,
-                "interestRate": interestRate,
-                "overDraftPenalty": overDraftPenalty,
-                "requiredMinimumBalance": requiredMinimumBalance,
-                "isMinimumBalanceRequired": isMinimumBalanceRequired,
-                "recurringTransactions": null,
-                "accountHistory": null
-            },
+            "balance": balance,
+            "accountType": accountType,
+            "interestRate": interestRate,
+            "overDraftPenalty": overDraftPenalty,
+            "requiredMinimumBalance": requiredMinimumBalance,
+            "isMinimumBalanceRequired": isMinimumBalanceRequired,
+            "recurringTransactions": null,
+            "accountHistory": null
+        };
+
+        this.http.post(this.url, this.data).subscribe(response => {
+            this.responseData = response.json();
+            console.log(this.responseData);
+            this.id = this.responseData.id;
+        });
+    }
+
+    getInterestValue(id:any, interval:number, frequency:number,interestType:number, calculationRule:number, numDaysForRule:number) {
+        this.data = {
+            "id": id,
             "interval": interval,
             "frequency": frequency,
-            "interestType": interestRate,
+            "interestType": interestType,
             "calculationRule": calculationRule,
             "numDaysForRule": numDaysForRule
         };
-
-        console.log("hi John");
         
         this.http.post(this.url, this.data).subscribe(response => {
             this.responseData = response.json();

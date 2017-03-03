9a32abd220654283ee788e46b3a34103d07ef676
John Collins <johncollins@zipcoders-MacBook-Pro-4.local>
2017-03-03 18:43:04 -0500

    interestType toggle works

--- src/pages/complex/complex.ts
+++ src/pages/complex/complex.ts
@@ -13,7 +13,16 @@ import { NavController, NavParams, AlertController } from 'ionic-angular';
 })
 export class ComplexPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {}
+accountID:number = 27;
+interval:number = 365;
+frequency:number = 12;
+interestType:any = "SIMPLE";
+calculationRule:any = "NONE";
+numDaysForRule:number = 0;
+checked:boolean;
+complexInterest:any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ComplexPage');
@@ -27,5 +36,14 @@ export class ComplexPage {
     });
     alert.present()
   }
+  getInterestValue(){
+    if(this.complexInterest){this.interestType="COMPLEX";}else{this.interestType="SIMPLE";}
+    console.log(this.accountID, this.interval, this.frequency, this.interestType, this.calculationRule, this.numDaysForRule, );
+  }
+  updateInterestType(){
+    this.interestType="COMPLEX";
+    console.log(this.complexInterest);
+  }
 
 }
+
